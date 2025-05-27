1c77b61bc4164463b3e49dfe7316a5ddde12fde3
Aecrimx <mircea.alexandru.popa@gmail.com>
2025-05-27 22:28:01 +0300

    Create spline_liniar.py

---
+++ optimised_functions/spline_liniar.py
@@ -0,0 +1,13 @@
+import numpy as np
+#codu lu Robert
+
+def spline_liniar(X, Y, x):
+    y_val = np.zeros_like(x)
+    indices = np.searchsorted(X, x, side='right') - 1
+    indices = np.clip(indices, 0, len(X) - 2)
+    x0, x1 = X[indices], X[indices+1]  # coefficients
+    y0, y1 = Y[indices], Y[indices+1]
+    
+    y_val = y0 + (y1 - y0) / (x1 - x0) * (x - x0)
+
+    return y_val
